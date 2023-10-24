37081b1c063d6c68189c02411bc67cd0e59aaa85
Li Guangpu <lguangpu001.@gmail.com>
2023-10-24 19:21:08 +0800

    1

---
+++ timer_group/main/src/hal/ds_timer.c
@@ -0,0 +1,195 @@
+/************************************************************
+ * ds_timer.c : ESP32的硬件定时器处理模块
+ * 
+ * 此模块提供了ESP32的定时器初始化、中断处理等功能，支持触摸屏和UI更新的计时任务。
+ * 
+ ************************************************************/
+
+// 标准输入输出头文件
+# include <stdio.h>
+
+// ESP32的相关头文件
+# include "esp_types.h"
+# include "driver/periph_ctrl.h"
+# include "driver/timer.h"
+# include "esp_log.h"
+
+// FreeRTOS的任务调度和队列相关头文件
+# include "freertos/FreeRTOS.h"
+# include "freertos/task.h"
+# include "freertos/queue.h"
+
+// 与本模块相关的头文件
+# include "ds_timer.h"
+// # include "ds_system_data.h"
+// # include "ds_ft6336.h"
+// # include "ds_ui_timepage.h"
+// # include "ds_ui_tomatpage.h"
+
+// 定时器的一些预定义参数
+# define TIMER_DIVIDER                          16
+# define TIMER_SCALE                            (TIMER_BASE_CLK / TIMER_DIVIDER/1000)
+# define TIMER_INTERVALO_SEC                    (10)
+# define TEST_WITH_RELOAD                       1
+
+// 定时器事件的数据结构
+typedef struct {
+    uint64_t timer_minute_count;
+    uint64_t timer_second_count;
+    uint64_t timer_second_count_isr;
+}timer_event_t;
+
+// 全局的定时器事件变量
+timer_event_t g_timer_event;
+
+// 定义与定时器和UI更新相关的队列
+xQueueHandle timer_queue;
+// xQueueHandle ui_update_timer_queue;
+
+/*
+ * @description : 定时器中断服务例程，当定时器达到其警报值时触发
+ *
+ * @param : para - 传递给中断服务例程的参数，这里是定时器的索引
+ *
+ * @return : 无
+ */
+
+void IRAM_ATTR timer_group0_isr(void *para)
+{
+    // 获取定时器的自旋锁，防止其他任务或中断干扰定时器的配置
+    timer_spinlock_take (TIMER_GROUP_0);
+
+    // 将传递的参数转换为定时器索引
+    int timer_idx = (int) para;
+
+    // 准备基本的事件数据，稍后将发送回主程序任务
+    timer_event_t evt;
+
+    // 清除定时器的中断状态，这样它不会立即再次触发
+    timer_group_clr_intr_status_in_isr (TIMER_GROUP_0, TIMER_0);
+
+    // 警报触发后，我们需要再次启用它，以便下次触发
+    timer_group_enable_alarm_in_isr(TIMER_GROUP_0, timer_idx);
+
+    // 现在将事件数据发送回主程序任务
+    // xQueueSendFromISR 是 FreeRTOS 的函数，用于在中断服务例程中向队列发送数据
+    // timer_queue 是之前创建的队列，evt 是要发送的数据，最后一个参数用于接收被挂起任务的标识，这里不需要所以使用 NULL
+    xQueueSendFromISR (timer_queue, &evt, NULL);
+
+    // 释放定时器的自旋锁
+    timer_spinlock_give(TIMER_GROUP_0);
+}
+
+
+/*
+ * @description : 初始化指定的定时器，并配置其基本参数
+ *
+ * @param : timer_idx - 要初始化的定时器编号
+ * @param : auto_reload - 定时器到达警报值时是否自动重装
+ * @param : timer_interval_sec - 要设置的警报间隔
+ *
+ * @return : 无
+ */
+static void tg0_timer_init(int timer_idx, bool auto_reload, double timer_interval_sec)
+{
+    // 定义定时器的配置结构体
+    timer_config_t config = {
+        .divider = TIMER_DIVIDER,                           // 计数器时钟分频器。分频器的范围从 2 到 65536
+        .counter_dir = TIMER_COUNT_UP,                      // 设置计数器递增
+        .counter_en = TIMER_PAUSE,                          // 计数器使能
+        .alarm_en = TIMER_ALARM_EN,                         // 定时器报警使能
+        .auto_reload = auto_reload,                         // 定时器自动重新加载
+    };
+
+    // 使用上面的配置初始化定时器
+    timer_init (TIMER_GROUP_0, timer_idx, &config);
+
+    // 定时器的计数器将从下面的值开始
+    // 此外，如果设置了auto_reload，这个值将在警报时自动重载
+    timer_set_counter_value(TIMER_GROUP_0, timer_idx, 0x00000000ULL);
+
+    // 配置警报值和警报中断
+    // TIMER_SCALE 是一个宏，用于将秒转换为定时器的内部计数单位
+    timer_set_alarm_value (TIMER_GROUP_0, timer_idx, timer_interval_sec * TIMER_SCALE);
+
+    // 使能定时器的中断
+    timer_enable_intr (TIMER_GROUP_0, timer_idx);
+
+    // 为定时器注册一个中断服务例程
+    // timer_group0_isr 是中断服务例程的函数地址
+    // ESP_INTR_FLAG_IRAM 是中断的属性标志
+    timer_isr_register(TIMER_GROUP_0, timer_idx, timer_group0_isr, (void *) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
+
+    // 启动定时器
+    timer_start(TIMER_GROUP_0, timer_idx);
+}
+
+
+/*
+ * @description :  定时器事件处理任务。该任务监听定时器事件队列并更新分钟和秒的计数器。
+ *                每60秒刷新时间，并每1秒执行一次相关操作
+ *
+ * @param : arg - 任务参数，当前实现未使用
+ *
+ * @return : 无
+ */
+static void timer_evt_task(void *arg)
+{
+    while (1)
+    {
+        timer_event_t evt;
+
+        // 从队列中接收定时器事件
+        xQueueReceive (timer_queue, &evt, portMAX_DELAY);
+
+        // 更新分钟计数器
+        g_timer_event.timer_minute_count ++;
+        
+        // 每6000次事件（相当于60秒）重置分钟计数器并刷新时间
+        if (g_timer_event.timer_minute_count >= 6000)
+            g_timer_event.timer_second_count = 0;
+        
+        // 更新秒计数器
+        g_timer_event.timer_second_count ++;
+
+        // 每100次事件（相当于1秒）重置秒计数器并执行相关操作
+        if(g_timer_event.timer_second_count >= 100)
+        {
+            g_timer_event.timer_second_count = 0;
+            ESP_LOGI (TAG, "is run");
+        }
+    }
+}
+
+/*
+ * @description : 示例程序的UI更新任务，每10ms执行一次
+ *
+ * @param : arg - 任务参数，当前实现未使用
+ *
+ * @return : 无
+ */
+
+/*
+ * @description : 初始化定时器相关的资源
+ *
+ * @return : 无
+ */
+void ds_timer_init (void)
+{
+    // 重置定时器的分钟，秒和中断服务例程的秒计数器为0
+    g_timer_event.timer_minute_count = 0;
+    g_timer_event.timer_second_count = 0;
+    g_timer_event.timer_second_count_isr = 0;
+
+    // 创建一个长度为10，每个元素大小为timer_event_t的队列
+    // 该队列可能用于存储定时器事件
+    timer_queue = xQueueCreate (10, sizeof(timer_event_t));    
+
+    // 初始化定时器，设置其属性如是否自动重载和时间间隔                     
+    example_tg0_timer_init (TIMER_0, TEST_WITH_RELOAD, TIMER_INTERVALO_SEC);
+
+    // 创建一个新的任务来处理定时器事件
+    // 任务名为"timer_evt_task"，堆栈大小为2048，优先级为5
+
+    xTaskCreate (timer_evt_task, "timer_evt_task", 2048, NULL, 5, NULL);
+}
--- 基础版_CN.md
+++ 基础版_CN.md
@@ -1,92 +1,42 @@
-[English](基础班_EN.md) | 中文
-# 任务创建基础版
+（由于任务量有点大，所以先不出英文版的了）
+# 定时器基础版
 
 ## 基础知识
 
-### 什么是任务（Task）?
+### 什么是定时器（Timer）?
 
-任务就是你想让你的电脑或者说单片机做的一件事。就像你给自己安排了一个任务，比如说做作业。在编程的世界里，任务就是一段程序，它能完成一些特定的工作。
+定时器就像一个闹钟，可以在特定的时间点提醒我们做某件事情。在编程世界里，定时器可以帮助我们在特定的时间执行特定的代码。
 
-### 为什么要创建任务?
+### 为什么要使用定时器?
 
-你可能想让你的单片机同时做很多事情，比如说同时检查温度和显示时间。为了能让单片机同时做多件事，你就需要创建不同的任务。
+有时候，我们需要在特定的时间做某些事情，比如每隔1秒更新一次屏幕显示的时间。定时器可以帮我们精确地控制代码何时执行。
 
-### 什么是FreeRTOS?
+### 什么是硬件定时器？
 
-FreeRTOS是一个“实时操作系统”。它能帮你管理和运行你创建的任务。就像一个班长，帮你安排和管理班级的事务。
-
-### 任务是怎样运行的？
-
-当你创建了一个任务，FreeRTOS就会帮你运行它。任务运行，就是它开始做它应该做的事情。比如说，打印消息、检查按钮是否被按下等。
-
-### 怎样创建一个任务？
-
-在FreeRTOS里，创建任务很简单，只需要调用一个函数xTaskCreate()就可以了。
-
-### 如何结束一个任务？
-
-如果你想让一个任务停下来，你可以调用vTaskDelete()函数。这个函数告诉FreeRTOS，你想让这个任务停下来。
+硬件定时器是单片机（比如ESP32）内部的一个特殊装置，可以精确计算时间，并在达到我们设置的时间点时，通知我们的程序。
 
 ## 示例代码
-~~~c
-# include <stdio.h>
-# include "sdkconfig.h"
-# include "freertos/FreeRTOS.h"
-# include "freertos/task.h"
-# include "esp_system.h"
-# include "esp_spi_flash.h"
 
-# define CHIP_NAME "ESP32"
+[示例代码main.c](timer_group/main/timer_group_example_main.c)  
+[示例代码timer.c](timer_group/main/src/hal/ds_timer.c)
 
-static const char *TAG = "MAIN APP";
+### 示例代码解释
 
-static void test_task_example (void* arg)
-{
-    for(;;) 
-    {
-        vTaskDelay (1000 / portTICK_PERIOD_MS);
+这是一个基于ESP32和FreeRTOS的一个定时器应用示例。我们将从ds_timer.c和主程序app_main两部分来解释定时器的基础知识。
 
-        printf ("task run \n");
-    }
-}
+1. 定时器初始化（ds_timer_init函数）:  
+首先，程序通过ds_timer_init函数初始化定时器。在这个函数里，我们创建了一个队列timer_queue，用于存储定时器事件，并调用example_tg0_timer_init函数初始化定时器。
 
-void app_main(void)
-{
-    printf("Hello world!\n");
+2. 定时器配置（tg0_timer_init函数）:  
+在tg0_timer_init函数里，我们配置了定时器的一些基本参数，比如计数方向、是否自动重载等，并设置了定时器的报警值。当定时器计数到这个值时，它会触发一个中断。
 
-    esp_chip_info_t chip_info;
-    esp_chip_info (&chip_info);
-    printf ("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-            CHIP_NAME,
-            chip_info.cores,                                                
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",            
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");         
+3. 定时器中断服务例程（timer_group0_isr函数）:  
+当定时器触发中断时，timer_group0_isr函数会被调用。在这个函数里，我们清除了定时器的中断状态，并将定时器事件发送到timer_queue队列。
 
-    printf ("silicon revision %d, ", chip_info.revision);                   
+4. 处理定时器事件（timer_evt_task函数）:  
+timer_evt_task函数是一个任务，它不断从timer_queue队列中读取定时器事件，并根据事件更新了分钟和秒的计数器。
 
-    printf ("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+5. 主程序（app_main函数）:  
+在主程序app_main函数里，我们首先打印了一些芯片信息，然后调用ds_timer_init函数初始化定时器，并创建了一个简单的任务test_task_example，这个任务每隔1秒打印一条消息。
 
-    printf ("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
-
-    ESP_LOGI (TAG, "system init V1.1");
-
-    xTaskCreate (test_task_example, "test_task_example", 2048, NULL, 10, NULL);
-
-    while(1)
-    {
-        printf ("system run ...\n");                    
-        vTaskDelay (1000 / portTICK_PERIOD_MS);        
-    }
-}
-~~~
-
-### 解析
-
-1. **任务创建**：在 app_main 函数中，有一行代码 xTaskCreate(test_task_example, "test_task_example", 2048, NULL, 10, NULL);。这行代码告诉 FreeRTOS 创建一个新的任务，并且运行 test_task_example 函数。
-2. **任务函数**：test_task_example 函数是一个简单的示例任务。这个函数包含一个无限循环，它会做两件事情：
-- 使用 vTaskDelay 函数让任务休眠1秒。
-- 使用 printf 函数打印 "task run" 消息。
-3. **任务延迟**：vTaskDelay(1000 / portTICK_PERIOD_MS); 这行代码让任务休眠了1秒。这样，任务会每隔1秒打印一次消息。
-
-通过以上的解释和示例代码的分析，希望你能对任务创建有一个基本的理解。你可以试着运行示例代码，看看代码运行的效果，也可以尝试修改代码，创建你自己的任务，看看会发生什么。
+通过以上的解释和示例代码的分析，希望你能对定时器有一个基本的理解。你可以试着运行示例代码，看看代码运行的效果，也可以尝试修改代码，创建你自己的定时器，看看会发生什么。
